863120c6d2a0f43b098cde2f41f0fe6579972e82
Abdelnasser Fried <abdenacer@cureety.com>
2023-03-21 15:53:45 +0100

    testing the CI

--- moderation/index.js
+++ moderation/index.js
@@ -12,7 +12,7 @@ app.post("/events", async (req, res) => {
   const { type, data } = req.body;
 
   if (type === "CommentCreated") {
-    const status = data.content.includes("orange") ? "rejected" : "approved";
+    const status = data.content.includes("apple") ? "rejected" : "approved";
     await axios.post(`${process.env.REACT_EVENT_BUS}/events` || "http://localhost:4005/events", {
       type: "CommentModerated",
       data: { id: data.id, content: data.content, postId: data.postId, status },
